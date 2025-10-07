355f845c8ae70a5bdab4b925b7258ec6e96b6048
Vedant094 <raturi.vedant003@gmail.com>
2025-10-07 12:27:15 +0530

    changes made by v

--- src/main/java/com/coforge/training/onlinebanking/repository/AccountRepository.java
+++ src/main/java/com/coforge/training/onlinebanking/repository/AccountRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.coforge.training.onlinebanking.model.Account;
 
-public interface AccountUser extends JpaRepository<Account, Long>{
+public interface AccountRepository extends JpaRepository<Account, Long>{
 
 }
